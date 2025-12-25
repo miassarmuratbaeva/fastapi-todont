7b08e983b2c309464264af3fecd2dbd967c86702
miassarmuratbaeva <miassarmuratbaeva47@gmail.com>
2025-12-25 23:09:49 +0500

    done

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,28 +1,54 @@
 from fastapi.routing import APIRouter
+from fastapi import FastAPI , HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
+app = FastAPI()
+tags = ["tasks"]
+
+class Task(BaseModel):
+    id: int
+    title: str
+    descriptional: Optional[str] = None
+    done: bool = False
+
+tasks = []
+
+@router.post("/" , response_model=Task)
+def create_task(task: Task):
+    tasks.append(task)
+    return task
 
 
-@router.post("/")
-def create_task():
-    pass
-
-
-@router.get("/")
+@router.get("/" , response_model=List[Task])
 def get_task_list():
-    pass
+    return tasks
 
-
-@router.get("/{pk}")
+@router.get("/{pk}", response_model=Task)
 def get_one_task(pk: int):
-    pass
+    for task in tasks:
+        if task.id == pk:
+            return task
+    raise
+HTTPException(status_code=404, detail="Task not found")
 
 
-@router.put("/{pk}")
-def update_task():
-    pass
-
+@router.put("/{pk}" , response_model=Task)
+def update_task(pk: int, updated_task: Task):
+    for index, task in enumerate(tasks):
+        if task.id == pk:
+            tasks[index] = updated_task
+            return updated_task
+    raise 
+HTTPException(status_code=404, detail="Task not found")
 
 @router.delete("/{pk}")
-def delete_task():
-    pass
+def delete_task(pk: int):
+    for index, task in enumerate(tasks):
+        if task.id == pk:
+            del tasks[index]
+            return {"detail": "Task deleted"}
+    raise HTTPException(status_code=404, detail="Task not found")
+
+app.include_router(router)
